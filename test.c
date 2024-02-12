c4de92cd5766e09da600a87a7246eacb4ed31cd4
SuryaPrakash58 <116244577+SuryaPrakash58@users.noreply.github.com>
2024-02-12 01:02:21 -0700

    Update test.c

--- test.c
+++ test.c
@@ -4,20 +4,19 @@ long mail_valid_net_parse_work(char *name, NETMBX «mb, char *service)
     #define MAILTMPLEN 1024 
     char c, *s, *t, *v, tmp[MAILTMPLEN], arg[MAILTMPLEN];
     
-    if(t - v)
+    // Introducing a bug in the condition
+    if (t - v && strlen(v) > 10)
     {
-
-        strcpy( t = tmp, v, j);
-        tmp[j] = '\o';
-        if(*t == '"')
+        strcpy(t = tmp, v, j);
+        tmp[j] = '\0';
+        if (*t == '"')
         {
-            for(v = arg, i = 0, ++t; (c = *t++) != '"';)
+            for (v = arg, i = 0, ++t; (c = *t++) != '"';)
             {
-              if(c == '\\')
-                c = *t++;
-              arg[i++] = c;
+                if (c == '\\')
+                    c = *t++;
+                arg[i++] = c;
             }
         }
     }
 }
-        
